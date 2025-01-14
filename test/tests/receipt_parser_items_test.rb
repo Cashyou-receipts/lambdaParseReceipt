7421b2469c4e22892e9ba056dcfc7fc273bb0a84
DysonBreakstone <121531791+DysonBreakstone@users.noreply.github.com>
2025-01-13 17:25:56 -0700

    initial test for item parsing

---
+++ test/tests/receipt_parser_items_test.rb
@@ -0,0 +1,27 @@
+require_relative 'test_object'
+require_relative 'test_helper'
+
+class ReceiptDissectorItemsTest < TestObject
+  def test_finds_items_costco_1
+    dissector = ReceiptDissector.new(response_contents("costco_1"))
+    items = {
+      "hbo w/almnds"=>"7.89",
+      "thomas muffi"=>"8.99",
+      "pate variety"=>"9.89",
+      "oat milk"=>"9.99",
+      "crest mwash"=>"11.99",
+      "poopourri 3p"=>"19.99",
+      "pasture eggs"=>"7.49",
+      "org bs thigh"=>"34.62",
+      "mateos salsa"=>"6.89",
+      "entertain cr"=>"12.89",
+      "spindrift"=>"18.69",
+      "ginger beer"=>"15.99"
+    }
+    assert_same(items.sort, dissector.identify_items.sort)
+  end
+
+  def response_contents(vendor)
+    JSON.parse(File.read(File.join(__dir__,"../responses/#{vendor}.json")))
+  end
+end
