bda03deea39e7b6cbc0d55dd4f7c6b1415bf1381
DysonBreakstone <121531791+DysonBreakstone@users.noreply.github.com>
2025-01-16 18:45:29 -0700

    test running instructions

--- test/tests/test_helper.rb
+++ test/tests/test_helper.rb
@@ -8,4 +8,8 @@ Dir.glob(File.join(__dir__, "/*.rb")).each do |file|
   require_relative file unless file.include?('test_object.rb')
 end
 
+# paste the following lines into an irb session:
+# require './test/tests/test_runner'
 # TestRunner.test "ReceiptDissectorItemsTest"
+# TestRunner.test(*args)
+# require './test/tests/test_runner'; TestRunner.test "ReceiptDissectorItemsTest"
